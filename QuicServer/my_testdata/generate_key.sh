9fe75780611ac72f5e40ba8ca1beeff26f18530d
HaskyFynn <haskyfynn101@gmail.com>
2024-07-01 13:54:09 +0000

    update

--- QuicServer/my_testdata/generate_key.sh
+++ QuicServer/my_testdata/generate_key.sh
@@ -9,7 +9,7 @@ echo "Generating CSR"
 openssl req -out cert.csr -new -newkey rsa:2048 -nodes -keyout priv.key -subj "/O=quic-go/"
 
 echo "Sign certificate:"
-openssl x509 -req -sha256 -days 3650 -in cert.csr -out cert.pem -CA ca.pem -CAkey ca.key -CAcreateserial -extfile <(printf "subjectAltName=DNS:localhost,IP:10.10.10.47")
+openssl x509 -req -sha256 -days 3650 -in cert.csr -out cert.pem -CA ca.pem -CAkey ca.key -CAcreateserial -extfile <(printf "subjectAltName=DNS:localhost,IP:10.10.10.45")
 
 # debug output the certificate
 sudo openssl x509 -noout -text -in cert.pem
